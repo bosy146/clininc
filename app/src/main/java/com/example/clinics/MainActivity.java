bf28b5bee30e247d36a2d4c2138535bdea8e08a9
ibrahim <himahamed999@gmail.com>
2020-03-27 15:30:48 +0200

    hima

--- app/src/main/java/com/example/clinics/MainActivity.java
+++ app/src/main/java/com/example/clinics/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.clinics;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
-//    NavigationView navigationView;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar ( toolbar );
 
         drawerLayout = findViewById ( R.id.drawar );
-//        navigationView = findViewById ( R.id.navigation_view );
+        navigationView = findViewById ( R.id.navigation_view );
 
         actionBarDrawerToggle = new ActionBarDrawerToggle ( this, drawerLayout, toolbar, R.string.open, R.string.close );
+        actionBarDrawerToggle.getDrawerArrowDrawable ().setColor ( ContextCompat.getColor ( this, R.color.white ) );
         drawerLayout.addDrawerListener ( actionBarDrawerToggle );
         actionBarDrawerToggle.setDrawerIndicatorEnabled ( true );
         actionBarDrawerToggle.syncState ();
