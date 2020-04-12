68bfba7ea07abbe997617306f6e8bc93510054ed
ibrahim <himahamed999@gmail.com>
2020-04-12 20:22:18 +0200

    hima

--- app/src/main/java/com/example/clinics/adapter/wordadapter.java
+++ app/src/main/java/com/example/clinics/adapter/wordadapter.java
@@ -17,60 +17,65 @@ import java.util.ArrayList;
 public class wordadapter extends RecyclerView.Adapter<wordadapter.WordViewHolder> {
     public ArrayList<wordclass> mArrayList;
     private OnItemClickListener mlistener;
-    public interface OnItemClickListener{
+
+    public interface OnItemClickListener {
         void onItemClick(int position);
     }
-    public void SetOnItemClickListener (OnItemClickListener listener){
-        mlistener=listener;
+
+    public void SetOnItemClickListener(OnItemClickListener listener) {
+        mlistener = listener;
     }
-    public static class WordViewHolder extends RecyclerView.ViewHolder
-    {
+
+    public static class WordViewHolder extends RecyclerView.ViewHolder {
 
         public ImageView mImageView;
         public TextView mTextView;
 
         public WordViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
-            super(itemView);
-            mImageView=itemView.findViewById( R.id.imageView);
-            mTextView=itemView.findViewById(R.id.text);
-            itemView.setOnClickListener(new View.OnClickListener() {
+            super ( itemView );
+            mImageView = itemView.findViewById ( R.id.imageView );
+            mTextView = itemView.findViewById ( R.id.text );
+            itemView.setOnClickListener ( new View.OnClickListener () {
                 @Override
                 public void onClick(View view) {
-                    if (listener!=null){
-                        int position=getAdapterPosition();
-                        if (position!=RecyclerView.NO_POSITION){
-                            listener.onItemClick(position);
+                    if (listener != null) {
+                        int position = getAdapterPosition ();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick ( position );
                         }
                     }
 
                 }
-            });
+            } );
         }
 
     }
-    public wordadapter (ArrayList<wordclass>list)
-    {
-        mArrayList=list;
+
+    public wordadapter(ArrayList<wordclass> list) {
+        mArrayList = list;
 
     }
+
     @Override
     public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
-        WordViewHolder wordViewHolder=new WordViewHolder(v,mlistener);
+        View v = LayoutInflater.from ( parent.getContext () ).inflate ( R.layout.list_item, parent, false );
+        WordViewHolder wordViewHolder = new WordViewHolder ( v, mlistener );
         return wordViewHolder;
     }
+
     @Override
     public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
-        wordclass currenItem = mArrayList.get(position);
+        wordclass currenItem = mArrayList.get ( position );
         //pass values
 
-        holder.mImageView.setImageResource(currenItem.getmImageView());
-        holder.mTextView.setText(currenItem.getmTextV1());
-       // holder.mTextView2.setText(currenItem.getmTextV2());
+        holder.mImageView.setImageResource ( currenItem.getmImageView () );
+        holder.mTextView.setText ( currenItem.getmTextV1 () );
+        // holder.mTextView2.setText(currenItem.getmTextV2());
 
     }
+
     @Override
     public int getItemCount() {
-        return mArrayList.size();
+        return mArrayList.size ();
     }
 }
