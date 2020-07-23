3d175f217444bd21e0801b711bc9c67ca3701041
ibrahim <himahamed999@gmail.com>
2020-07-23 09:43:52 +0200

    hima

--- app/src/main/java/com/example/clinics/Click.java
+++ app/src/main/java/com/example/clinics/Click.java
@@ -5,24 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Click extends AppCompatActivity {
 
-
+    Spinner daySp, hourSp;
+    EditText phoneEd, emailEd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_click);
-        Intent intent=getIntent();
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_click );
+        Intent intent = getIntent ();
 
+        initViews ();
+    }
 
+    void initViews() {
+        daySp = findViewById ( R.id.day_spinner );
+        hourSp = findViewById ( R.id.hour_spinner );
+        phoneEd = findViewById ( R.id.phone_edit_text );
+        emailEd = findViewById ( R.id.email_edit_text );
     }
 
     public void click(View view) {
-        Toast.makeText ( this, "تم الحجز", Toast.LENGTH_SHORT ).show ();
+
+        if (daySp.getSelectedItemPosition () == 0) {
+            Toast.makeText ( this, "لم يتم اختيار اليوم", Toast.LENGTH_SHORT ).show ();
+        } else if (phoneEd.getText ().length () == 0) {
+            Toast.makeText ( this, "لم يتم ادخال رقم الهاتف", Toast.LENGTH_SHORT ).show ();
+        }
+
+
+//        Toast.makeText ( this, "تم الحجز", Toast.LENGTH_SHORT ).show ();
 
     }
 }
--- app/src/main/java/com/example/clinics/gym/gymb.java
+++ app/src/main/java/com/example/clinics/gym/gymb.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -13,18 +14,39 @@ import com.example.clinics.R;
 public class gymb extends AppCompatActivity {
 
 
-
+    Spinner departmentSp, daySp, hourSp;
+    EditText phoneEd, emailEd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gymb);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_gymb );
 
-
+        initViews ();
     }
 
+    void initViews() {
+        departmentSp = findViewById ( R.id.department_spinner );
+        daySp = findViewById ( R.id.day_spinner );
+        hourSp = findViewById ( R.id.hour_spinner );
+        phoneEd = findViewById ( R.id.phone_edit_text );
+        emailEd = findViewById ( R.id.email_edit_text );
+    }
 
     public void click(View view) {
-        Toast.makeText ( this, "تم الحجز", Toast.LENGTH_SHORT ).show ();
+
+        if (departmentSp.getSelectedItemPosition () == 0) {
+            Toast.makeText ( this, "لم يتم اختيار القسم", Toast.LENGTH_SHORT ).show ();
+        } else if (daySp.getSelectedItemPosition () == 0) {
+            Toast.makeText ( this, "لم يتم اختيار اليوم", Toast.LENGTH_SHORT ).show ();
+        } else if (hourSp.getSelectedItemPosition () == 0) {
+            Toast.makeText ( this, "لم يتم اختيار الساعة", Toast.LENGTH_SHORT ).show ();
+        } else if (phoneEd.getText ().length () == 0) {
+            Toast.makeText ( this, "لم يتم ادخال رقم الهاتف", Toast.LENGTH_SHORT ).show ();
+        } else if (emailEd.getText ().length () == 0) {
+            Toast.makeText ( this, "لم يتم ادخال البريد الاكترونى", Toast.LENGTH_SHORT ).show ();
+        } else {
+            Toast.makeText ( this, "تم الحجز", Toast.LENGTH_SHORT ).show ();
+        }
     }
 }
